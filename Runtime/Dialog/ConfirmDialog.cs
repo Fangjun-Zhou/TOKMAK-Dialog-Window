e0be39579fdc2a67c44a463381e26ac04244d1f3
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-09-03 02:41:26 -0500

    Added: Info Dialog

---
+++ Runtime/Dialog/ConfirmDialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NaughtyAttributes;
+using TMPro;
+using UnityEngine;
+
+namespace FinTOKMAK.DialogWindow.Runtime.Dialog
+{
+    public class ConfirmDialog : MonoBehaviour
+    {
+        #region Public Field
+
+        public Action onConfirm;
+
+        [BoxGroup("UI Components")]
+        public TMP_Text title;
+
+        [BoxGroup("UI Components")]
+        public TMP_Text content;
+
+        #endregion
+
+        /// <summary>
+        /// Confirm button clicked.
+        /// </summary>
+        public void Confirm()
+        {
+            onConfirm?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Runtime/DialogManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using FinTOKMAK.DialogWindow.Runtime.Dialog;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace FinTOKMAK.DialogWindow.Runtime
+{
+    public class DialogManager : MonoBehaviour
+    {
+        #region Singleton
+
+        public static DialogManager Instance = null;
+
+        #endregion
+
+        #region Public Field
+
+        [BoxGroup("Dialog Prefabs")]
+        public ConfirmDialog confirmDialog;
+
+        #endregion
+
+        private void Awake()
+        {
+            // Register singleton.
+            if (Instance != null)
+            {
+                Destroy(this);
+                return;
+            }
+            Instance = this;
+            
+            // Make singleton don't destroy on load.
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            Destroy(this);
+            Instance = null;
+        }
+
+        #region Dialog Methods
+
+        public IEnumerator ShowInfoDialog(LocalizedString title, LocalizedString content)
+        {
+            // Create dialog
+            ConfirmDialog dialog = Instantiate(confirmDialog, gameObject.transform);
+            bool hasConfirmed = false;
+
+            // Setup dialog
+            void OnTitleChanged(string title)
+            {
+                dialog.title.text = title;
+            }
+
+            void OnContentChanged(string content)
+            {
+                dialog.content.text = content;
+            }
+            
+            dialog.title.text = title.GetLocalizedString();
+            title.StringChanged += OnTitleChanged;
+            dialog.content.text = content.GetLocalizedString();
+            content.StringChanged += OnContentChanged;
+
+            dialog.onConfirm += () =>
+            {
+                title.StringChanged -= OnTitleChanged;
+                content.StringChanged -= OnContentChanged;
+
+                hasConfirmed = true;
+            };
+
+            while (!hasConfirmed)
+            {
+                yield return null;
+            }
+        }
+
+        #endregion
+    }
+}
